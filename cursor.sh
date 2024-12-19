e792be60dc746dd3a83e906564d756127a06de6c
Ninad Naik <ninadnaik07@gmail.com>
2024-12-19 15:44:02 +0530

    Use USER variable in cursor script

--- cursor.sh
+++ cursor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$1" = "." ]; then
-    cursor_path=$(find /home/ninad/Applications -name "cursor-*.AppImage" | head -n 1)
+    cursor_path=$(find /home/${USER}/Applications -name "cursor-*.AppImage" | head -n 1)
     "$cursor_path" "$(pwd)" &>/dev/null & disown
 else
-    cursor_path=$(find /home/ninad/Applications -name "cursor-*.AppImage" | head -n 1)
+    cursor_path=$(find /home/${USER}/Applications -name "cursor-*.AppImage" | head -n 1)
     "$cursor_path" "$@" &>/dev/null & disown
 fi
